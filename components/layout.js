dee9574a68f4b9a0ea7383e1eb146dd6dd3e4852
Eric Foster <>
2020-09-12 11:20:22 -0400

    fix layout export to properly apply container styling

--- components/layout.js
+++ components/layout.js
@@ -1,7 +1,5 @@
 import styles from './layout.module.css'
 
-function Layout({ children }) {
+export default function Layout({ children }) {
     return <div className={styles.container}>{children}</div>
 }
-
-export default Layout
